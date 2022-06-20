c907e5f9ab97980bc716a6419c32d15e5e3230d4
Tomasz Królicki <tomek.krolicki@gmail.com>
2022-06-20 14:02:17 +0200

    new user

---
+++ src/Codecool.CodecoolShop/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Codecool.CodecoolShop.Models
+{
+    public class User
+    {
+        public User(string name, int userId, List<Product> shoppingCart)
+        {
+            Name = name;
+            UserId = userId;
+            ShoppingCart = shoppingCart;
+
+        }
+        public string Name { get; set; }
+        public int UserId { get; set; }
+        public List<Product> ShoppingCart { get; set; }
+
+    }
+}
--- src/Codecool.CodecoolShop/Startup.cs
+++ src/Codecool.CodecoolShop/Startup.cs
@@ -74,6 +74,10 @@ namespace Codecool.CodecoolShop
             productDataStore.Add(new Product { Name = "Amazon Fire", DefaultPrice = 49.9m, Currency = "USD", Description = "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", ProductCategory = tablet, Supplier = amazon });
             productDataStore.Add(new Product { Name = "Lenovo IdeaPad Miix 700", DefaultPrice = 479.0m, Currency = "USD", Description = "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", ProductCategory = tablet, Supplier = lenovo });
             productDataStore.Add(new Product { Name = "Amazon Fire HD 8", DefaultPrice = 89.0m, Currency = "USD", Description = "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", ProductCategory = tablet, Supplier = amazon });
+            User Janusz = new User ( "Janusz", 1, new List<Product>() );
+            Janusz.ShoppingCart.Add(new Product { Name = "Amazon Fire", DefaultPrice = 49.9m, Currency = "USD", Description = "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", ProductCategory = tablet, Supplier = amazon });
+            Console.WriteLine(1);
+
         }
     }
 }

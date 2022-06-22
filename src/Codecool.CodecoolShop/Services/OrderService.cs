01e2f5d8e78aa41aeaf9a15beb891c035e3bd825
Hubert <hubertkrzysztonekk@gmail.com>
2022-06-22 14:49:24 +0200

    + IAllOrdersDao

---
+++ src/Codecool.CodecoolShop/Services/OrderService.cs
@@ -0,0 +1,19 @@
+﻿using Codecool.CodecoolShop.Daos;
+using Codecool.CodecoolShop.Models;
+
+namespace Codecool.CodecoolShop.Services
+{
+    public class OrderService
+    {
+        private readonly IAllOrdersDao orderDao;
+        public OrderService(IAllOrdersDao orderDao)
+        {
+            this.orderDao = orderDao;
+        }
+
+        public Order GetOrder(int orderId)
+        {
+            return this.orderDao.Get(orderId);
+        }
+    }
+}

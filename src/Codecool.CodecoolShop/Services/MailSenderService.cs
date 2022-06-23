d0dc83ec2b8e5b2af5f1356f9d132a83fb58e9a1
Michal Prosniak <michalprosniak@gmail.com>
2022-06-23 14:51:21 +0200

    email message

--- src/Codecool.CodecoolShop/Services/MailSenderService.cs
+++ src/Codecool.CodecoolShop/Services/MailSenderService.cs
@@ -4,6 +4,7 @@ using System.Net.Mail;
 using System.Net.Mime;
 using System.Threading;
 using System.ComponentModel;
+using System.Linq;
 using Codecool.CodecoolShop.Models;
 
 namespace Codecool.CodecoolShop.Services
@@ -30,7 +31,30 @@ namespace Codecool.CodecoolShop.Services
 
         private string CreateMessage(OrderForDelete order)
         {
-            return $"{order.OrderId} {order.FirstName}";
+            string products = "";
+            for (int i = 0; i < order.ShoppingCart.Count; i++)
+            {
+                products += $"Product {i + 1}: " + order.ShoppingCart[i].Name + "\n";
+                products += "Supplier: " + order.ShoppingCart[i].Supplier.Name + "\n";
+                products += "Price for 1: " + order.ShoppingCart[i].DefaultPrice + "$\n";
+                products += "Quantity: " + order.ShoppingCart[i].Quantity + "\n\n";
+            }
+
+            string isPayed = "";
+            if (order.IsPayed)
+            {
+                isPayed = "You already payed for products!\n\n";
+            }
+            else
+            {
+                isPayed = $"Remember - You will have to pay {order.ShoppingCartValue}$ when the package will arrive!\n\n";
+            }
+            string userData =
+                $"Your personal data: {order.FirstName} {order.LastName}\nEmail: {order.Email}\nPhone: {order.PhoneNumber}\n" +
+                $"Your billing address: {order.BillingZip} {order.BillingCity}, {order.BillingCountry}\n{order.BillingAddress}\n" +
+                $"Your shipping address: {order.ShippingZip} {order.ShippingCity}, {order.ShippingCountry}\n{order.ShippingAddress}\n\n";
+            return $"ORDER NUMBER {order.OrderId}\n\n\n" +
+                   "Your shop list:\n" + products + userData + isPayed + "CodeCoolShop";
         }
     }
 }
